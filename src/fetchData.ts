ff35ce84f17bc45b3927b1612096485cb4a763f0
vmcustodio <van.custodio.vc@gmail.com>
2022-12-17 17:26:20 -0300

    first commit

---
+++ src/fetchData.ts
@@ -0,0 +1,11 @@
+export default async function fetchData<T>(url: string): Promise<T | null>{
+  try {
+    const response = await fetch(url)
+    if(!response.ok) throw new Error("Erro: " + response.status)
+    const json = await response.json()
+    return json
+  } catch(error) {
+      if(error instanceof Error) console.log('fetchData: ' + error.message)
+      return null
+  }
+}
