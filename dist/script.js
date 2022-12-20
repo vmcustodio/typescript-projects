0ff698c8ddfe05c3a95e0957545705a3ac0eedbc
vmcustodio <van.custodio.vc@gmail.com>
2022-12-19 23:47:18 -0300

    feat: add bestday

--- dist/script.js
+++ dist/script.js
@@ -28,6 +28,10 @@ function preencherEstatisticas(transacoes) {
             currency: "BRL",
         });
     }
+    const diaElement = document.querySelector("#dia span");
+    if (diaElement) {
+        diaElement.innerText = data.melhorDia[0];
+    }
 }
 function preencherTabela(transacoes) {
     const tabela = document.querySelector("#transacoes tbody");
--- src/Estatisticas.ts
+++ src/Estatisticas.ts
@@ -11,11 +11,15 @@ export default class Estatisticas {
   total;
   pagamento;
   status;
+  semana;
+  melhorDia;
   constructor(transacoes: Transacao[]) {
     this.transacoes = transacoes
     this.total = this.setTotal()
     this.pagamento = this.setPagamento()
     this.status = this.setStatus()
+    this.semana = this.setSemana()
+    this.melhorDia = this.setMelhorDia()
   }
 
   private setTotal() {
@@ -33,5 +37,35 @@ export default class Estatisticas {
 
   private setStatus() {
     return countBy(this.transacoes.map(({ status }) => status))
+  }
+
+  private setSemana() {
+    const semana = {
+      domingo: 0,
+      segunda: 0,
+      terca: 0,
+      quarta: 0,
+      quinta: 0,
+      sexta: 0,
+      sabado: 0,
+    }
+    for (let i= 0; i < this.transacoes.length; i++) {
+      const day = this.transacoes[i].data.getDay()
+      if (day === 0) semana.domingo += 1
+      if (day === 1) semana.segunda += 1
+      if (day === 2) semana.terca += 1
+      if (day === 3) semana.quarta += 1
+      if (day === 4) semana.quinta += 1
+      if (day === 5) semana.sexta += 1
+      if (day === 5) semana.sabado += 1
+    }
+    console.log(semana)
+    return semana
+  }
+
+  private setMelhorDia() {
+    return Object.entries(this.semana).sort((a,b) => {
+      return b[1] - a[1]
+    })[0]
   }
 }
